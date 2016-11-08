352b5ef78d565f49bf3945f634dce17e014e22a7
David House <davidhousedev@gmail.com>
2016-11-08 09:33:17 -0500

    feat: Implement initial logic for adding and retrieving artists to/from database

--- database_controller.py
+++ database_controller.py
@@ -1,3 +1,8 @@
+import urllib2
+import json
+import pprint # remove for production
+import datetime
+
 # Initializes python shell to interface with database
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,5 +15,36 @@ Base.metadata.bind = engine
 
 # Establish database connection session
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
+def db_add_artist(spotify_id):
+    """ Queries Spotify by artist id for artist info and top tracks
+    then adds artist to appropriate database tables """
+    url = 'https://api.spotify.com/v1/artists/%s' % spotify_id
+    data = json.load(urllib2.urlopen(url))
+    pprint.pprint(data)
+    print 'name is %s' % data[u'name'].lower()
+
+    artist_name = data[u'name']
+    new_artist = Artist(name=artist_name,
+                        spotify_id=spotify_id,
+                        url_name=artist_name.lower(),
+                        created=datetime.datetime.utcnow())
+
+    session = DBSession()
+    try:
+        session.add(new_artist)
+        session.commit()
+        artist = artist_by_spotify_id(session, spotify_id)
+        print 'Added artist: %s' % artist.name
+    except Exception, e:
+        session.rollback()
+        raise e
+
+def artist_by_spotify_id(session, spotify_id):
+    """ Queries database for artist and returns if found """
+    spotify_id = unicode(spotify_id)
+    try:
+        artist = session.query(Artist).filter_by(spotify_id=spotify_id).one()
+        return artist
+    except Exception, e:
+        raise e
