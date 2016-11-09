d04fc4022835cddae71a2172fcee435e12b00e2c
David House <davidhousedev@gmail.com>
2016-11-09 18:05:36 -0500

    feat: Implement basic user feedback when adding artists to database

--- static/js/artist_create.js
+++ static/js/artist_create.js
@@ -19,7 +19,9 @@ $(function(){
                                             'class': 'artist'});
                 var addListener = function(id){
                     $artistDiv.click(function(){
-                        createArtist(artists[id].id)
+                        createArtist($(this),
+                                     artists[id].id,
+                                     artists[id].name)
                     })
                 }($artistDiv.attr('id')); //add current artist event listener
                 $artistDiv.text(artists[i].name);
@@ -36,16 +38,51 @@ function searchError(err){
     console.log('Error: ' + err);
 }
 
-function createArtist(spotifyId){
+function createArtist(element, spotifyId, name){
+    element.hide();
+    var status = $(document.createElement('li'));
+    status.attr('id', spotifyId);
+    status.css('display', 'block');
+    console.log(status);
+    status.text('Adding ' + name + '...');
+    $('#artists-added').append(status);
+
     $.ajax({
         type: 'POST',
         url: '/artist/create/',
         data: spotifyId,
+        error: function(){
+            artistAddError(status, element);
+        },
         success: function(data){
-            console.log(data);
-            alert('successfully added artist');
+            json = JSON.parse(data)
+            addingSuccessful(status, json, name);
         }
-    })
+    });
+}
+
+// This function is called while an artist is being added to the database
+// Displays a status message informing user that database is currently working
+function addingArtist(element, name){
+    console.log(element);
+    element.append('Adding ' + name + '...');
+}
+
+function addingSuccessful(element, json, name){
+    console.log(json);
+    element.text('Successfully added ' + name);
+    var artistLink = $(document.createElement('a'));
+    artistLink.attr('href', json.artist_url);
+    artistLink.append('View artist');
+    console.log(artistLink)
+    element.append('<br>');
+    element.append(artistLink)
+}
+
+function artistAddError(status, searchResult){
+    status.hide();
+    searchResult.show();
+    searchResult.append(' Unable to add');
 }
 
 // refresh with: window.location.reload(true)
