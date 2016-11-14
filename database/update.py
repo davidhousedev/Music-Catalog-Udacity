6f49d9e0762b211f13c6430644aec40149082758
David House <davidhousedev@gmail.com>
2016-11-14 10:21:58 -0500

    feat: Update artist url name when artist name is updated

--- database/update.py
+++ database/update.py
@@ -1,11 +1,13 @@
 from sqlalchemy import update
 from database_setup import Base, Artist, ArtistGenre, Genre
 from database_setup import Influence, TopSongs
+from database.database_helpers import url_name
 
 def artist(session, name, art_id):
     print 'trying to update %s, id: %s' % (name, art_id)
     artist = session.query(Artist).filter_by(art_id=art_id).first()
     artist.name = name
+    artist.url_name = url_name(name)
     session.add(artist)
 
 def artist_genres(session):
