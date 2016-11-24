0140ca75efa11cd20ce9ff64e65ec032c9e92a3a
David House <davidhousedev@gmail.com>
2016-11-24 10:43:43 -0500

    feat: Prevent unauthenticated and unauthorized users from editing and deleting artists and genres on the backend

--- application.py
+++ application.py
@@ -54,6 +54,13 @@ def artist(artist):
            methods=['GET', 'POST'])
 def artist_create():
     """ Renders and processess form for creating artists """
+    # Verify that the current user has permission to perform this action
+    try:
+        assert login_session['user_id']
+    except:
+        flash('You must be logged in to do that')
+        return redirect(url_for('show_login'))
+
     if request.method == 'POST':
         form_data = request.form
         for item in form_data:
@@ -78,6 +85,17 @@ def artist_create():
 def artist_edit(artist):
     """ Edit database entry of a specific artist """
     artist = db.db_get_artist(parse_url(artist))
+
+    # Verify that the current user has permission to perform this action
+    try:
+        assert login_session['user_id']
+    except:
+        flash('You must be logged in to do that')
+        return redirect(url_for('show_login'))
+    if login_session['user_id'] != artist['user']:
+        flash("You cannot edit another user's artist")
+        return redirect(url_for('artist', artist=artist['url_name']))
+
     if request.method == 'POST':
         form_data = parse_edit_form_data(request.form)
         pprint.pprint(form_data)
@@ -101,6 +119,17 @@ def artist_edit(artist):
 def artist_delete(artist):
     """ Delete an artist from the database """
     artist = db.db_get_artist(artist)
+
+    # Verify that the current user has permission to perform this action
+    try:
+        assert login_session['user_id']
+    except:
+        flash('You must be logged in to do that')
+        return redirect(url_for('show_login'))
+    if login_session['user_id'] != artist['user']:
+        flash("You cannot delete another user's artist")
+        return redirect(url_for('artist', artist=artist['url_name']))
+
     if request.method == 'POST':
         db.db_delete_artist(artist['art_id'])
         return redirect(url_for('catalog'))
@@ -125,6 +154,13 @@ def genre(genre):
            methods=['GET', 'POST'])
 def genre_create():
     """ Create a new genre in the database """
+    # Verify that the current user has permission to perform this action
+    try:
+        assert login_session['user_id']
+    except:
+        flash('You must be logged in to do that')
+        return redirect(url_for('show_login'))
+
     artists = db.db_get_all_artists()
     genres = db.db_get_all_genres()
     if request.method == 'POST':
@@ -148,6 +184,17 @@ def genre_create():
 def genre_edit(genre):
     """ Edit a specific genre """
     genre, gen_artists, gen_influences = db.db_get_genre(parse_url(genre))
+
+    # Verify that the current user has permission to perform this action
+    try:
+        assert login_session['user_id']
+    except:
+        flash('You must be logged in to do that')
+        return redirect(url_for('show_login'))
+    if login_session['user_id'] != genre.user:
+        flash("You cannot edit another user's genre")
+        return redirect(url_for('genre', genre=genre.url_name))
+
     if request.method == 'POST':
         form_data = parse_genre_form_data(request.form)
         db.db_edit_genre(form_data['name'],
@@ -183,6 +230,17 @@ def genre_edit(genre):
 def genre_delete(genre):
     """ Delete a specific genre """
     genre = db.db_get_genre(parse_url(genre))[0]
+
+    # Verify that the current user has permission to perform this action
+    try:
+        assert login_session['user_id']
+    except:
+        flash('You must be logged in to do that')
+        return redirect(url_for('show_login'))
+    if login_session['user_id'] != genre.user:
+        flash("You cannot delete another user's genre")
+        return redirect(url_for('genre', genre=genre.url_name))
+
     if request.method == 'POST':
         db.db_delete_genre(genre.gen_id)
         return redirect(url_for('catalog'))
@@ -195,6 +253,7 @@ def show_login():
                     for char in xrange(32))
     login_session['state'] = state
     return render_template('loginv2.html', STATE=state, cur_user=login_session)
+
 
 @app.route('/gconnect',
            methods=['POST'])
@@ -210,11 +269,13 @@ def authenticate_user():
         flow.redirect_uri = 'postmessage'
         credentials = flow.step2_exchange(id_token)
     except client.FlowExchangeError:
-        response = make_response(json.dumps('Failed to upgrade authorization code.'), 401)
+        response = make_response(
+            json.dumps('Failed to upgrade authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    url = (
+        'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
     http = httplib2.Http()
     result = json.loads(http.request(url, 'GET')[1])
     if result.get('error') is not None:
@@ -223,17 +284,20 @@ def authenticate_user():
         return response
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        response = make_response(json.dumps("Token's user ID doesn't match given user ID"), 401)
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID"), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     if result['issued_to'] != GOOGLE_CLIENT_ID:
-        response = make_response(json.dumps("Token's client ID does not match app's."), 401)
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps("Current user is already connected."), 200)
+        response = make_response(
+            json.dumps("Current user is already connected."), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -258,7 +322,6 @@ def authenticate_user():
         db_user = db.db_create_user(login_session)
     login_session['user_id'] = db_user.user_id
 
-
     return redirect(url_for('catalog'))
 
 
@@ -266,7 +329,8 @@ def authenticate_user():
 def disconnect_user():
     access_token = login_session.get('access_token')
     if access_token is None:
-        response = make_response(json.dumps("Current user not connected."), 401)
+        response = make_response(
+            json.dumps("Current user not connected."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
@@ -286,9 +350,6 @@ def disconnect_user():
         response.headers['Content-Type'] = 'application/json'
         return response
     return redirect(url_for('catalog'))
-
-
-
 
 
 if __name__ == '__main__':
